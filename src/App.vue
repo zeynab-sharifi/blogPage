94b70182d7701ef0f2d98bfef8a6eade184b55e6
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-02 03:26:46 -0800

    create components blog page

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,35 @@
 <script >
-
+import NavBlog from './components/NavBlog.vue'
+import HeaderBlog from'./components/lo/HeaderBlog.vue'
+import ListPost from './components/ListPost.vue'
+import PaginationBlog from './components/lo/PaginationBlog.vue'
+import FooterBlog from './components/lo/FooterBlog.vue'
+  export default{
+    components :{
+    NavBlog,
+    HeaderBlog,
+    ListPost,
+    PaginationBlog,
+    FooterBlog
+}
+  }
 </script>
 
 <template>
         <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container">
-                <a class="navbar-brand" href="#!">Blog page</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-  <header class="py-5 bg-header border-bottom mb-4">
-            <div class="container">
-                <div class="text-center my-5">
-                    <h1 class="fw-bolder">Welcome to Blog Home!</h1>
-                    <p class="lead mb-0">Add blog page. Welcome to Blog Home</p>
-                </div>
-            </div>
-        </header>
+        <nav-blog></nav-blog>
+        <!-- header blog page -->
+        <header-blog></header-blog>
         <div class="col-lg-12">
                 <!-- Featured blog post-->
                <div class="container">
-                  <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
-                <!-- Nested row for non-featured blog posts-->
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
-    <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
-    <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
-                    </div>
-                </div>
+                  <list-post></list-post>
                </div>
                 <!-- Pagination-->
-                <nav aria-label="Pagination">
-                    <hr class="my-0" />
-                    <ul class="pagination justify-content-center my-4">
-                        <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a></li>
-                        <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#!">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#!">3</a></li>
-                        <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
-                        <li class="page-item"><a class="page-link" href="#!">15</a></li>
-                        <li class="page-item"><a class="page-link" href="#!">Older</a></li>
-                    </ul>
-                </nav>
+                <pagination-blog></pagination-blog>
             </div>
              <!-- Footer-->
-    <footer class="py-5 bg-dark">
-        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
-    </footer>
+            <footer-blog></footer-blog>
 </template>
 
 <style>
---
+++ src/components/lo/HeaderBlog.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="py-5 bg-header border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Welcome to Blog Home!</h1>
+                    <p class="lead mb-0">Add blog page. Welcome to Blog Home</p>
+                </div>
+            </div>
+        </header>
+</template>

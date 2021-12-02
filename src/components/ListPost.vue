3440689a2f584715c7ff6f8131625fabe70c602d
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-02 10:38:27 -0800

    add post component and import in listPost

--- src/components/ListPost.vue
+++ src/components/ListPost.vue
@@ -1,54 +1,22 @@
 <template>
-    <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
+    <post></post>
                 <!-- Nested row for non-featured blog posts-->
                 <div class="row">
                     <div class="col-lg-6">
-                        <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
-    <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
+                        <post></post>
+    <post></post>
                     </div>
                     <div class="col-lg-6">
-                        <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
-    <div class="card mb-4">
-        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
-        <div class="card-body">
-            <div class="small text-muted">January 1, 2021</div>
-            <h2 class="card-title h4">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-            <a class="btn btn-danger" href="#!">Read more →</a>
-        </div>
-    </div>
+                        <post></post>
+    <post></post>
                     </div>
                 </div>
 </template>
+<script>
+import Post from './Post.vue'
+export default {
+    components:{
+        Post
+    }
+}
+</script>

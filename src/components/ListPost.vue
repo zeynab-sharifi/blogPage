338d619b28bded307baf04f5fa6aea9cab1f8df7
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-02 12:48:52 -0800

    add loading post

--- src/components/ListPost.vue
+++ src/components/ListPost.vue
@@ -1,16 +1,21 @@
 <template>
-    <post></post>
-                <!-- Nested row for non-featured blog posts-->
-                <div class="row">
-                    <div class="col-lg-6">
-                        <post></post>
-    <post></post>
-                    </div>
-                    <div class="col-lg-6">
-                        <post></post>
-    <post></post>
-                    </div>
+<!-- loading post -->
+    <div v-if="!loading">
+        <post></post>
+        <!-- Nested row for non-featured blog posts-->
+            <div class="row">
+                <div class="col-lg-6">
+                    <post></post>
+                    <post></post>
                 </div>
+                <div class="col-lg-6">
+                    <post></post>
+                    <post></post>
+                </div>
+            </div>
+    </div>
+    <!-- loading post -->
+    <div class="loader" v-else></div>
 </template>
 <script>
     import axios from 'axios'
@@ -26,8 +31,12 @@
             }
         },
         created(){
+            this.loading = true;
             axios.get('https://jsonplaceholder.typicode.com/posts?_page1&_limit=9')
-            .then(res => console.log(res))
+            .then(res => {
+                this.loading=false;
+                console.log(res.data)
+            })
             .catch(err => console.log(err))
         }
     }

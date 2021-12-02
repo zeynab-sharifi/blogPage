b40e2871dcf101f3e575d1946bbbeb6341f4b936
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-02 11:55:38 -0800

    add axios

--- src/components/ListPost.vue
+++ src/components/ListPost.vue
@@ -13,10 +13,22 @@
                 </div>
 </template>
 <script>
-import Post from './Post.vue'
-export default {
-    components:{
-        Post
+    import axios from 'axios'
+    import Post from './Post.vue'
+    export default {
+        components:{
+            Post
+        },
+         data(){
+            return{
+                posts:null,
+                loading:false
+            }
+        },
+        created(){
+            axios.get('https://jsonplaceholder.typicode.com/posts?_page1&_limit=9')
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+        }
     }
-}
 </script>
